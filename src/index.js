aa47f47aa93f92231d468acc9e1d1e8a280c7774
shahrukhnizami <nizamishahrukh12@gmail.com>
2025-04-21 16:01:39 +0500

    Solve dot env problem

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import router from "./routes/index.js";
-dotenv.config({
-    path : "./env"
-})
+dotenv.config()
 const app  = express()  
 
 app.use(cors({
