afaeca667f563b9e52ab322c59d88f8335b46379
shahrukhnizami <154953847+shahrukhnizami@users.noreply.github.com>
2025-04-23 15:49:27 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app  = express()
 app.use(cors({
 
     // origin : ["https://kmh-front-end.vercel.app"],
-    origin : ["http://localhost:5173"],
+  origin : "*",
 
     credentials : true
 }))
