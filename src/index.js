3e7d2765ba670be33b22a9a4eff65a7e07f9bb9f
shahrukhnizami <154953847+shahrukhnizami@users.noreply.github.com>
2025-04-23 15:57:04 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 app.use(express.json())
 app.use(express.static("public"))
 app.use("/api/v1", router);
